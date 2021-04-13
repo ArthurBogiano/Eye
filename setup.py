580469845036af039719797a3558ce94307d0c4c
Arthur Bogiano <atacktody@gmail.com>
2021-04-13 11:45:00 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name = 'Eye Websocket Server',
+    version = '1.1.0',
+    author = 'Arthur Bogiano',
+    author_email = 'arthurprosel@gmail.com',
+    packages = ['eye'],
+
+    description = 'It is a utility to simplify service creation processes using websockets',
+
+    long_description = 'pt-br: É um utilitário para simplificar processos de criação de serviços utilizando websockets',
+
+    url = 'https://github.com/ArthurBogiano/eye',
+
+    project_urls = {
+        'Código fonte': 'https://github.com/ArthurBogiano/eye'
+    }
+)
